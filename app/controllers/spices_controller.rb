26d43cfaa1f2e326a980dccaf4d8ac8a3e29a29a
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-11-23 17:30:53 +0300

    completed mini-project-crud

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -1,33 +1,38 @@
 class SpicesController < ApplicationController
+  wrap_parameters format: []
 
-    def index
-        spices = Spice.all
-        render json: spices
-    end
+  def index
+    spices = Spice.all
+    render json: spices
+  end
 
-    def create 
-        spice = Spice.create(spice_params)
-        render json: spice, status: :created
-    end
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end
 
-    def update 
-        #find the id
-        spice = Spice.find_by(id: params[:id])
-        #update
-        spice.update(spice_params)
-        #render json data 
-        render json: spice, status: :created
-    end
+  def update
+    #find the id
+    spice = find_spice
+    #update
+    spice.update(spice_params)
+    #render json data
+    render json: spice, status: :created
+  end
 
-    def destroy
-        spice = Spice.find_by(id: params[:id])
-        spice.destroy
-        head :no_content
-    end
+  def destroy
+    spice = find_spice
+    spice.destroy
+    head :no_content
+  end
 
-    private 
+  private
 
-    def spice_params
-        params.permit(:title, :description, :image, :notes, :rating)
-    end
+  def spice_params
+    params.permit(:title, :description, :image, :notes, :rating)
+  end
+
+  def find_spice
+    Spice.find_by(id: params[:id])
+  end
 end
